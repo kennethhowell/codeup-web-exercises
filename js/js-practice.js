6c127c1104bb6f6ad659e6c922d283f776c5ede8
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-10-08 10:54:26 -0500

    in lecture practice and download of functions exercise

---
+++ js/js-practice.js
@@ -0,0 +1,19 @@
+// one multiplication and one division js function anon and named
+
+"use strict";
+
+function multiplyGuy(x , y) {
+    return x * y;
+}
+
+function divisionDude(x , y) {
+    return x / y;
+}
+
+var multiGuy = function(x , y) {
+    return x * y;
+};
+
+var diviDude =  function(x , y) {
+    return x / y;
+};
