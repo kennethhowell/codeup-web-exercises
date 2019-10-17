c7c615cd078aaad9362d25e3be92a7a8f8f05e2f
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-10-17 08:58:45 -0500

    morning git commit

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,11 +1,27 @@
-function showMultiplicationTable(num) {
-    var userNum = num;
-    var y = 1;
-    var result = userNum * y;
-    for (var i = 0; i < 10; i++) {
-        console.log((userNum + " x " + y + " = " + result));
-        y++;
-    }
-}
+// function showMultiplicationTable(num) {
+//     var userNum = num;
+//     var y = 1;
+//     for (var i = 0; i < 10; i++) {
+//         console.log((userNum + " x " + y + " = " + (userNum * y++)));
+//     }
+// }
+//
+// showMultiplicationTable(7);
 
-showMultiplicationTable(7);
+// var randomNum = Math.floor(Math.random() * (100 - 20) + 1) + 20;
+
+// for (var i = 0; i < 10; i++) {
+//     var randomNum = Math.floor(Math.random() * (100 - 20) + 1) + 20;
+//     if (randomNum % 2 === 0) {
+//         console.log((randomNum + " is your random number, and it is even!"));;
+//     } else {
+//         console.log((randomNum + " is your random number, and it is odd!"));;
+//     }
+// }
+
+//
+// for (var o = 0; o < 1000000000; o++) {
+//         if (o === 1 || o === 22 || o === 333 || o === 4444 || o === 55555 || o === 666666 || o === 7777777 || o === 88888888 || o === 999999999) {
+//             console.log(o);
+//         };
+//     }
--- js/while.js
+++ js/while.js
@@ -7,24 +7,24 @@
 // //     i = i * 2;
 // // }
 //
-// // while loop ice cream word problem ~ random num 50 - 100 = cones to sell, 1 - 5 = cones sold, return msg for status of inventory and sale
-//
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// var i = allCones;
-//
-//
-// do {
-//     var soldcones = Math.floor(Math.random() * 5) + 1;
-//
-//     if (i > 0 && i >= soldcones) {
-//         i = i - soldcones;
-//         console.log(soldcones + " cones were just sold! And you have " + i + " cones left!");
-//     } else if (soldcones > i) {
-//         console.log("You can't sell " + soldcones + " cones! You only have " + i + " cones left to sell!");
-//     }
-//
-//
-// } while (i > 0);
-//
-// console.log("You sold all the cones! Let's boogie!");
-//
+// while loop ice cream word problem ~ random num 50 - 100 = cones to sell, 1 - 5 = cones sold, return msg for status of inventory and sale
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var i = allCones;
+
+
+do {
+    var soldcones = Math.floor(Math.random() * 5) + 1;
+
+    if (i > 0 && i >= soldcones) {
+        i = i - soldcones;
+        console.log(soldcones + " cones were just sold! And you have " + i + " cones left!");
+    } else if (soldcones > i) {
+        console.log("You can't sell " + soldcones + " cones! You only have " + i + " cones left to sell!");
+    }
+
+
+} while (i > 0);
+
+console.log("You sold all the cones! Let's boogie!");
+
