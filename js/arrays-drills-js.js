a70bb82c668869a3d583f01172563e28b95dda2c
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-10-17 16:57:02 -0500

    begun the bonuses have

---
+++ js/arrays-drills-js.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+
+
+function allIndexesOf (array, search) {
+    var indices = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === search) indices.push(i);
+    }
+   return indices;
+}
+
+console.log(allIndexesOf(fruits, "apple"));
+console.log(allIndexesOf(fruits, "guava"));
+console.log(allIndexesOf(fruits, "pineapple"));
