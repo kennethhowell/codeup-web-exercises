145ef4d254993d64219a3b64981b047604c5eec4
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-10-29 11:56:00 -0500

    jquery lec and exer complete

---
+++ js/jquery_exercises.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$("li").css("font-size", "20px")
+
+// $("h1").css("background-color", "gold");
+// $("p").css("background-color", "gold");
+// $("li").css("background-color", "gold");
+
+    $("li, p, h1").css("background-color", "gold")
+
+// alert( $("h1").html());
+
+});
