99863ec08c5a4e65ee96633077409a8625b62e81
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-10-30 12:44:28 -0500

    middle of code project

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -38,3 +38,57 @@ $(document).ready(function() {
 
     );
 });
+
+// write a js function that will return an array of hamsters height ascending order
+
+var hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+// function ascendingHamsters(hamsters){
+//     var bucket = [];
+//     var i = 0;
+//     var testcase = 0;
+//     for (i; i < hamsters.length; i++){
+//        if (hamsters[i].heightInMM > testcase) {
+//            testcase = hamsters[i].heightInMM;
+//            bucket.push(hamsters[i]);
+//        } else if (hamsters[i].heightInMM < testcase && hamsters[i].heightInMM < hamsters[i - 1].heightInMM) {
+//            bucket.unshift(hamsters[i]);
+//        }
+//     }
+//     console.log(bucket);
+//     return bucket
+// }
+//
+// ascendingHamsters(hamsters);
