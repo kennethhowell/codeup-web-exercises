80d55f2c25ec3de91ceaa3ef0f2c21ce953ad50c
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-11-05 16:00:36 -0600

    current weather adding functionality for tomorrow forest

---
+++ js/weather_map.js
@@ -0,0 +1,39 @@
+(function (){
+    "use strict";
+
+
+var testget = $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/892be8803635af0b0c775e7113aefb51/29.4284,-98.4870")
+    var currentweather = "";
+    var tomweather = "";
+    var dayafttomweather = "";
+    testget.done(function(data){
+        var temp = parseInt(data.currently.temperature);
+        var humidity = parseInt(data.currently.humidity * 100);
+        var windspeed = parseInt(data.currently.windSpeed);
+        var bpressure = parseInt(data.currently.pressure);
+        currentweather += "<span class=\"interiortext\"> Currently it is " + temp + "°F</span>";
+        currentweather += "<span class=\"interiortext\"> Humidity: " + humidity + "%</span>";
+        currentweather += "<span class=\"interiortext\"> Wind Speed: " + windspeed + " knots</span>";
+        currentweather += "<span class=\"interiortext\"> Barometric Pressure: " + bpressure + "mb</span>";
+        currentweather += "<span class=\"interiortext\">" + data.minutely.summary + "</span>";
+        $("#today").append(currentweather);
+    })
+
+    testget.done(function(data){
+        var hightemp = parseInt(data.daily.data[1].temperatureHigh);
+        var lowtemp = parseInt(data.daily.data[1].temperatureLow);
+        var rainchance = parseInt(data.daily.data[1].precipProbability * 100);
+        var summation = data.daily.data[1].summary;
+        console.log(summation);
+
+        // tomweather += "<span class=\"interiortext\"> Currently it is " + temp + "°F</span>";
+        // tomweather += "<span class=\"interiortext\"> Humidity: " + humidity + "%</span>";
+        // tomweather += "<span class=\"interiortext\"> Wind Speed: " + windspeed + " knots</span>";
+        // tomweather += "<span class=\"interiortext\"> Barometric Pressure: " + bpressure + "mb</span>";
+        // tomweather += "<span class=\"interiortext\">" + data.minutely.summary + "</span>";
+        // $("#today").append(currentweather);
+    })
+
+
+
+})();
